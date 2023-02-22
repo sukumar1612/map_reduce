feac0d72bb491f5c508f415ae038db99e5433d00
Sukumar Ganesan <sukumar_ganesan@Sukumars-MacBook-Air.local>
2023-02-22 11:23:36 +0530

    added csv generator

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
 
     # todo
     # 1. make server asynchronous and make p2p server asynchronous (better performance for IO ops) -> done
-    # 2. add client side server with fastapi + messaging queue for handling tasks ? -> priority 4
+    # 2. add client facing server with fastapi + messaging queue for handling tasks ? -> priority 4
     # 3. make client with typer or something similar -> priority 3
     # 4. manage dependencies with poetry -> last
-    # 5. assign priority to worker client thread rather than p2p server thread -> priority 2
+    # 5. assign priority to worker client thread rather than p2p server thread -> priority 2 not required
     # 6. create docker file and orchestrate with kubernetes -> priority -> 1
